fc93eec475df14f3a98751c8aafdc02dec022a9f
Benedikt Reiser <benedikt.reiser@innoactive.de>
2015-07-26 14:48:31 +0200

    documentatioooon

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -24,10 +24,19 @@ const KEY_ESCAPE = 27;
 const TAB_ITEM_HEIGHT = 63;
 const COLLECTION_HEIGHT = 298;
 
+////////////////////
+// React elements //
+////////////////////
+
+/** React Element for one Tab in the list */
 var Tab = React.createClass({
+  /**
+   * handler function for clicks on this item / tab. will forward the event to the parent handler
+   * @param  {Event} e the click event
+   */
   clickHandler: function(e) {
     // forward click event on one tab item to the tab list
-    this.props.handleClick(e, this.props.data);
+    this.props.clickHandler(e, this.props.data);
   },
   render: function() {
     var classes = classNames({
@@ -48,16 +57,21 @@ var Tab = React.createClass({
   }
 });
 
+/** React Element for the list of tabs */
 var TabList = React.createClass({
-  handleClick: function(e, tabData) {
+  /**
+   * handler function for clicks on one tab item in the list, will forward the event to the parent handler
+   * @param  {Event} e the click event
+   */
+  clickHandler: function(e, tabData) {
     // forward the click event from any tab item to the parent application
-    this.props.handleItemClick(e, tabData);
+    this.props.itemClickHandler(e, tabData);
   },
   render: function() {
     var tabNodes = this.props.tabs.map(function(tab) {
       /*jshint ignore:start */
       return (
-        <Tab key={tab.id} data={tab} handleClick={this.handleClick} />
+        <Tab key={tab.id} data={tab} clickHandler={this.clickHandler} />
       );
       /*jshint ignore:end */
     }.bind(this));
@@ -71,7 +85,13 @@ var TabList = React.createClass({
   }
 });
 
+/** React Element for the input field / typeahead */
 var OpenAnyTabInput = React.createClass({
+  /**
+   * handler function for any input in the input field, will trigger filtering the tabs by forwarding the event 
+   * to the parent element
+   * @param  {Event} e the input event
+   */
   handleInput: function(e) {
     this.props.onInputChange(e.target.value);
   },
@@ -84,7 +104,11 @@ var OpenAnyTabInput = React.createClass({
   }
 });
 
+/** React Element - Main Element containing all other elements */
 var OpenAnyTab = React.createClass({
+  /**
+   * @return {Object} Initial component state
+   */
   getInitialState: function() {
     return {
       tabs: [],
@@ -92,17 +116,27 @@ var OpenAnyTab = React.createClass({
       activeTabIdx: 0
     };
   },
+  /**
+   * function called as soon as the component has been mounted, i.e. when rendering is done
+   * used to register event listeners and store references to rendered DOM Elements
+   */
   componentDidMount: function() {
     // load all currently open tabs, as soon as the popup is loaded
     this.loadTabs().finally(function() {
       // also make sure to listen on keydown events for navigation, as soon as we have all tabs
       // we need arrows for navigation and return to confirm
-      document.body.addEventListener('keydown', this.handleKeydownEvent);
+      document.body.addEventListener('keydown', this.keydownHandler);
     }.bind(this));
     // find the actual dom node of the tab list
     this._tablistDOMNode = this.refs.tablist.getDOMNode();
   },
-  handleKeydownEvent: function(e) {
+  /**
+   * handler function for any keydown event
+   * - switches to the currently selected tab on RETURN key
+   * - rotates between tabs in the list on UP and DOWN arrows
+   * @param  {Event} e the keydown event
+   */
+  keydownHandler: function(e) {
     if (e.keyCode === KEY_DOWN) {
       // prevent scrolling
       e.preventDefault();
@@ -114,10 +148,15 @@ var OpenAnyTab = React.createClass({
       this._highlightTabAtIndex(this.state.activeTabIdx - 1);
     } else if (e.keyCode === KEY_RETURN) {
       // forward this event to a submit
-      this.handleSubmit(e);
+      this.submitHandler(e);
     }
   },
-  handleSubmit: function(e) {
+  /**
+   * handler function for submit events on the openanytab form
+   * triggers a switch to the currently selected element
+   * @param  {Event} e the submit event
+   */
+  submitHandler: function(e) {
     if (e.nativeEvent) {
       // only try to stop the event from bubbling and triggering default actions if there is a matching native event
       e.preventDefault();
@@ -125,6 +164,10 @@ var OpenAnyTab = React.createClass({
     }
     helpers.switchToTab(this.state.tabs[this.state.activeTabIdx]).then(helpers.closePopup);
   },
+  /**
+   * helper function to load all available and opened tabs as soon as the component is rendered
+   * @return {Promise} returns a promise that will be resolved to the list of tabs
+   */
   loadTabs: function() {
     return helpers.loadTabs().then(function(tabs) {
       // store initial tabs and current tabs
@@ -136,7 +179,12 @@ var OpenAnyTab = React.createClass({
       this._highlightTabAtIndex(0);
     }.bind(this));
   },
-  handleInputChange: function(keyword) {
+  /**
+   * handler function for input events
+   * this function will trigger filtering the list according to the new keyword (input) any time the input changes
+   * @param  {String} keyword the contents of the input field
+   */
+  inputChangeHandler: function(keyword) {
     var tabs = helpers.findMatchingTabs(keyword, this.state._tabs);
     // set the tabs
     this._updateTabs(tabs).then(function() {
@@ -144,19 +192,25 @@ var OpenAnyTab = React.createClass({
       this._highlightTabAtIndex(0);
     }.bind(this));
   },
+  /**
+   * updates the internal state to display the updated list of tabs
+   * @param  {Array<chrome.tabs.Tab>} newTabs the list of tabs to be displayed
+   * @return {Promise}                        promise that will be resolved as soon as the internal state is updated
+   */
   _updateTabs: function(newTabs) {
     var deferred = q.defer();
     this.setState({
       tabs: newTabs
-    }, function() {
-      deferred.resolve();
-    });
+    }, deferred.resolve);
     return deferred.promise;
   },
-  _highlightTabAtIndex: function(idx, cb) {
-    if (typeof cb !== 'function') {
-      cb = function() {};
-    }
+  /**
+   * highlights the tab identified by its index in the list of tabs
+   * @param  {Number}   idx The index of the tab in the array of tabs
+   * @return {Promise}      promise that will be resolved as soon as the tab is highlighted
+   */
+  _highlightTabAtIndex: function(idx) {
+    var deferredHighlighting = q.defer();
     // index sanitization
     if (idx >= this.state.tabs.length) {
       idx = 0;
@@ -190,15 +244,21 @@ var OpenAnyTab = React.createClass({
         this._tablistDOMNode.scrollTop = (idx) * TAB_ITEM_HEIGHT;
       }
       // finally execute the callback
-      cb();
+      deferredHighlighting.resolve();
     }.bind(this));
+    return deferredHighlighting.promise;
   },
-  handleItemClick: function(e, tabData) {
+  /**
+   * handler function for any click on a item in the list of tabs
+   * @param  {Event} e                 the click event
+   * @param  {chrome.tabs.Tab} tabData the tab which has been clicked
+   */
+  itemClickHandler: function(e, tabData) {
     // determine index of clicked element
     var idx = this.state.tabs.indexOf(tabData);
-    this._highlightTabAtIndex(idx, function() {
+    this._highlightTabAtIndex(idx).then(function() {
       // highlight this item and do a submit
-      this.handleSubmit(e);
+      this.submitHandler(e);
     }.bind(this));
   },
   render: function() {
@@ -206,11 +266,11 @@ var OpenAnyTab = React.createClass({
     return (
       <div className="container">
         <div className="row">
-          <form className="col s12" onSubmit={this.handleSubmit} >
+          <form className="col s12" onSubmit={this.submitHandler} >
             <div className="row">
               <div className="input-field col s12">
                 <i className="material-icons prefix">search</i>
-                <OpenAnyTabInput onInputChange={this.handleInputChange} />
+                <OpenAnyTabInput onInputChange={this.inputChangeHandler} />
                 <label htmlFor="tabTypeAheadInput">Just type ahead ...</label>
               </div>
             </div>
@@ -218,7 +278,7 @@ var OpenAnyTab = React.createClass({
         </div>
         <div className="row">
           <div className="col s12">
-            <TabList ref="tablist" tabs={this.state.tabs} handleItemClick={this.handleItemClick}/>
+            <TabList ref="tablist" tabs={this.state.tabs} itemClickHandler={this.itemClickHandler}/>
           </div>
         </div>
       </div>
