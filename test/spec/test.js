031935f34e5820190db15e7cb2ab405b2b701f2f
Benedikt Reiser <benedikt.reiser@innoactive.de>
2015-07-26 14:18:07 +0200

    first simple tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,15 +1,19 @@
 (function () {
   'use strict';
 
+  /* global beforeEach */
+
   // helpers
   var helpers = require('../../app/scripts/helpers.js');
+  // fuse
+  var Fuse = require('../../app/bower_components/fuse.js/src/fuse.js');
   // assertions
   var expect = require('chai').expect;
 
   describe('Tab Search Algorithm', function () {
 
     /** @type {Array} list of tabs to be searched */
-    var tabs = [
+    var _tabs = [
       {
         title: 'Facebook',
         url: 'http://www.facebook.com'
@@ -23,19 +27,62 @@
         url: 'chrome://extensions'
       }
     ];
+    var tabs = [];
+    var fuse = null;
 
-    console.log(helpers);
-    console.log(tabs);
+    describe('should only match facebook', function () {
+      var keyword = 'face';
 
-    describe('using own regex', function () {
-      it('should work', function () {
-        expect(true).to.be.equal(true);
+      // reset tabs before each testcase
+      beforeEach(function () {
+        tabs = _tabs.slice();
+        fuse = new Fuse(tabs, {
+          keys: [
+            'title'
+          ]
+        });
+      });
+
+      it('should work with our regex', function () {
+        // use the helpers function to filter the tablist
+        var filtered = helpers.findMatchingTabs(keyword, tabs);
+        expect(filtered.length).to.be.equal(1, 'should only have one element');
+        expect(filtered[0].title).to.contain('Facebook');
+      });
+
+      it('should work with fuse', function () {
+        var filtered = fuse.search(keyword);
+        expect(filtered.length).to.be.equal(1, 'should only have one element');
+        expect(filtered[0].title).to.contain('Facebook');
       });
     });
 
-    describe('using fuzzy search / Fuse', function () {
-      it('should work', function () {
-        expect(true).to.be.equal(true);
+    describe('should match chai assertion and extensions, extensions rated higher', function () {
+      var keyword = 'sio';
+
+      // reset tabs before each testcase
+      beforeEach(function () {
+        tabs = _tabs.slice();
+        fuse = new Fuse(tabs, {
+          keys: [
+            'title'
+          ]
+        });
+      });
+
+      it('should work with our regex', function () {
+        // use the helpers function to filter the tablist
+        var filtered = helpers.findMatchingTabs(keyword, tabs);
+        expect(filtered.length).to.be.equal(2, 'should only have one element');
+        expect(filtered[0].title).to.contain('Extensions');
+        expect(filtered[1].title).to.contain('Chai');
+      });
+
+      it('should work with fuse', function () {
+        var filtered = fuse.search(keyword);
+        expect(filtered.length).to.be.equal(2, 'should only have one element');
+        expect(filtered[0].title).to.contain('Extensions');
+        expect(filtered[1].title).to.contain('Chai');
       });
     });
   });
